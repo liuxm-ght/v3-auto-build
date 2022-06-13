324ea60d5904329016652df07ed9a191ce91caa9
liuxm-ght <xiaoming.liu@things-matrix.com>
2022-06-13 16:26:22 +0800

    have change code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <p>测试前端自动化部署工具项目，现在已经更新，提交了代码到github</p>
   <span>现在在自动化部署项目，点击部署按钮，后看此项目是否更新为上面的文字</span>
+  <span>呵呵哈哈哈，我又更新了</span>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 

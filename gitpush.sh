6be49b6bb7bb1164de1123ffa5ef133855f1ad1b
liuxm-ght <xiaoming.liu@things-matrix.com>
2022-06-13 21:31:06 +0800

    have change code

---
+++ gitpush.sh
@@ -0,0 +1,8 @@
+echo '开始提交'
+
+git status
+git add .
+git commit -m 'have change code'
+git push
+
+echo '提交完成'
--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <p>测试前端自动化部署工具项目，现在已经更新，提交了代码到github</p>
   <span>现在在自动化部署项目，点击部署按钮，后看此项目是否更新为上面的文字</span>
   <span>呵呵哈哈哈，我又更新了</span>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Auto-Deploy"/>
 </template>
 
 <script>
